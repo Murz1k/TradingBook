bb40be3ff30bd40e8f0fa279e747b1ce6a06c58b
Max89701 <Max89701@DESKTOP-S7O5E5S>
2016-12-04 11:44:42 +0300

    Add project files.

---
+++ Murz1k.TradingBook.DAL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Murz1k.TradingBook.DAL
+{
+    public class Class1
+    {
+    }
+}
